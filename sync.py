59907a8bd5b2a18970e66c0a641527bf1db42c9d
ciminoV <ciminovincenzo1@gmail.com>
2023-03-04 17:27:27 +0100

    update sync script

--- sync.py
+++ sync.py
@@ -1,5 +1,4 @@
 import simplenote
-# import itertools
 import json
 import os
 
@@ -11,27 +10,28 @@ sn = simplenote.Simplenote(credentials[1], credentials[0]) # username and psw
 remote_index = sn.get_note_list()[0]
 remote_index = [x for x in remote_index if x['deleted'] == False] # Ignore trash
 
-# Open local notes
+# Update local notes
 with open("index.json", "r") as out_file:
     local_index = json.load(out_file)
 
-# TODO
-# Iterate over remote index
-# Check for updates if remote version > local version
-# Create new file if not present
 update_index = []
 for note in remote_index:
-    if [x for x in local_index if x["key"] == note["key"]]:
-        print("Check the version on this one {}".format(note["key"]))
-    else:
-        # Create the note otherwise
-        print("Gotta create this one")
+    key = note["key"]
+    title = note["content"].partition('\n')[0].replace('#','').lstrip()+".md"
+    version = note["version"]
+
+    # Check wheter the file doesn't already exist or it's been updated
+    update_note = [x for x in local_index if x["key"] == key]
+    if not update_note or int(update_note[0]["version"]) < version:
+        with open(title, "w") as out_file:
+            out_file.write(note["content"])
 
     update_index.append({
-        "key": note["key"],
-        "title": note["content"].partition('\n')[0].replace('#','').lstrip()+".md",
-        "version": str(note["version"])
+        "key": key,
+        "title": title,
+        "version": str(version)
     })
 
+# Update index file
 with open("index.json", "w") as out_file:
     json.dump(update_index, out_file, indent=1)
