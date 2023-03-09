88fb6bcdcd7ec2ddc2471fe739a6c66c6b93177b
ciminoV <ciminovincenzo1@gmail.com>
2023-03-09 09:40:54 +0100

    update update script: sync the index after note update/add

--- update.py
+++ update.py
@@ -1,28 +1,25 @@
 import simplenote
+import sync
 import json
 import sys
 import os
 
 try:
-    note = sys.argv[1]
+    # Full path of the note
+    note_path = sys.argv[1]
 
     # Authenticate
     credentials = os.popen("pass simplesync").read().splitlines()
     sn = simplenote.Simplenote(credentials[1], credentials[0])  # username and psw
 
-    # Get note file
-    with open(note, "r") as out_file:
+    # Open note file and notes index 
+    with open(note_path, "r") as out_file:
         content = out_file.readlines()
 
     with open("index.json") as out_file:
         local_index = json.load(out_file)
     
-    # TODO:
-    # check if the file already exists in the index and if so update remote note
-    # getting the note key and content
-    # update_note create a new note if the key is not given: can be used to add
-    # a new note
-    note = os.path.basename(os.path.normpath(note))
+    note = os.path.basename(os.path.normpath(note_path))
     update_note = [x for x in local_index if x["title"] == note]
     if update_note:
         key = update_note[0]["key"]
@@ -31,5 +28,9 @@ try:
     else:
         sn.add_note({"content": "".join(content)})
 
+    #TODO
+    # Sync entire index only if new file is added otherwise only entry updated
+    sync.sync_index(local_index,sn)
+
 except:
-    print("No note file provided")
+    print("No valid note file provided")
