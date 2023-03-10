3704ccc3a4fb43078cb930f6752bd5802e2d1627
ciminoV <ciminovincenzo1@gmail.com>
2023-03-10 17:19:42 +0100

    update sync script: handle duplicate files

--- sync.py
+++ sync.py
@@ -1,13 +1,9 @@
-# TODO:
-# Handle files with same title
-# Remove local notes trashed remote notes: don't ignore trash and add proper entry
-# in index.json
 import simplenote
 import json
 import os
 
-# Local notes path
-path = "/home/cimino/documents/notes/"
+# Local notes PATH
+PATH = "/home/cimino/documents/notes/"
 
 
 # Authenticate
@@ -16,27 +12,47 @@ def authenticate():
     return simplenote.Simplenote(credentials[1], credentials[0])  # username and psw
 
 
-# Check wheter the file doesn't already exist or it's been updated
-def sync_index(local_index, sn):
-    # Get remote notes
+# Sync local notes with remote notes
+def remote_sync(local_index, sn):
     remote_index = sn.get_note_list()[0]
     remote_index = [x for x in remote_index if x["deleted"] == False]  # Ignore trash
 
     update_index = []
     for note in remote_index:
         key = note["key"]
-        title = note["content"].partition("\n")[0].replace("#", "").lstrip() + ".md"
+        title = (
+            note["content"].partition("\n")[0].replace("#", "").lstrip() or "new_note"
+        ) + ".md"
         version = note["version"]
+
+        # Check for duplicate remote notes
+        if update_index:
+            duplicate = [x for x in update_index if title in x["title"]]
+
+            if not duplicate:
+                num = ""
+            else:
+                num = "(%i)" % (len(duplicate))
+
+            title = title + "%s" % num
 
         update_note = [x for x in local_index if x["key"] == key]
         if not update_note or int(update_note[0]["version"]) < version:
-            with open(path + title, "w") as out_file:
+            with open(PATH + title, "w") as out_file:
                 out_file.write(note["content"])
 
         update_index.append({"key": key, "title": title, "version": str(version)})
 
-    # Update local notes
-    with open("index.json", "w") as out_file:
+    # Check for deleted remote notes
+    delete_note = [
+        i
+        for i in local_index + update_index
+        if i not in local_index or i not in update_index
+    ]
+    for delete in delete_note:
+        os.remove(PATH + delete["title"])
+
+    with open("./index.json", "w") as out_file:
         json.dump(update_index, out_file, indent=1)
 
 
@@ -44,7 +60,7 @@ if __name__ == "__main__":
     sn = authenticate()
 
     # Open local notes
-    with open("index.json", "r") as out_file:
+    with open("./index.json", "r") as out_file:
         local = json.load(out_file)
 
-    sync_index(local,sn)
+    remote_sync(local, sn)
