829aed61c3987e1de3e063b38d5bd6865de24368
ciminoV <ciminovincenzo1@gmail.com>
2023-03-10 19:41:14 +0100

    update update script: sync only when new note is added

--- update.py
+++ update.py
@@ -1,36 +1,27 @@
-import simplenote
 import sync
 import json
 import sys
 import os
 
-try:
-    # Full path of the note
-    note_path = sys.argv[1]
+# Full path of the note
+note_path = sys.argv[1]
 
-    # Authenticate
-    credentials = os.popen("pass simplesync").read().splitlines()
-    sn = simplenote.Simplenote(credentials[1], credentials[0])  # username and psw
+# Authenticate
+sn = sync.authenticate();
 
-    # Open note file and notes index 
-    with open(note_path, "r") as out_file:
-        content = out_file.readlines()
+# Open note file and notes index 
+with open(note_path, "r") as out_file:
+    content = out_file.readlines()
 
-    with open("index.json") as out_file:
-        local_index = json.load(out_file)
-    
-    note = os.path.basename(os.path.normpath(note_path))
-    update_note = [x for x in local_index if x["title"] == note]
-    if update_note:
-        key = update_note[0]["key"]
+with open("index.json") as out_file:
+    local_index = json.load(out_file)
 
-        sn.update_note({"key": key, "content": "".join(content)})
-    else:
-        sn.add_note({"content": "".join(content)})
+note = os.path.basename(os.path.normpath(note_path))
+update_note = [x for x in local_index if x["title"] == note]
+if update_note:
+    key = update_note[0]["key"]
 
-    #TODO
-    # Sync entire index only if new file is added otherwise only entry updated
-    sync.sync_index(local_index,sn)
-
-except:
-    print("No valid note file provided")
+    sn.update_note({"key": key, "content": "".join(content)})
+else:
+    sn.add_note({"content": "".join(content)})
+    sync.remote_sync(local_index,sn)
